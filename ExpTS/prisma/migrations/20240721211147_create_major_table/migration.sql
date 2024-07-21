445852710397e2de12b5f8538eb14db62cade606
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2024-07-21 17:38:27 -0400

    Express avançado - Exercicio 1

---
+++ ExpTS/prisma/migrations/20240721211147_create_major_table/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE `example_table` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `majors` (
+    `id` CHAR(40) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `code` CHAR(4) NOT NULL,
+    `description` TEXT NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `majors_name_key`(`name`),
+    UNIQUE INDEX `majors_code_key`(`code`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
